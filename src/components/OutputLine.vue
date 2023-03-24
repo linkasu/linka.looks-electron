733e594a3e6bcf682dc2994aab916fcc96567a1e
Ivan Bakaidov <ibakaidov@ya.ru>
2023-03-24 17:41:11 +0300

    add basic for eyetracker button

---
+++ src/components/OutputLine.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-layout full-height class="output-line">
+        <eye-button>
+            <v-icon>
+                mdi-lock
+            </v-icon>
+        </eye-button>
+        <eye-button>
+            <h1>text</h1>
+        </eye-button>
+        <eye-button @click="backspace">
+            <v-icon>
+                mdi-backspace
+            </v-icon>
+        </eye-button>
+        <eye-button>
+            <v-icon>
+                mdi-delete
+            </v-icon>
+        </eye-button>
+    </v-layout>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
+import EyeButton from '@/components/EyeButton.vue'
+@Options({
+    components:{
+        EyeButton
+    }
+})
+export default  class OutpuiLine extends Vue {
+
+    backspace(){
+        console.log('hi ')
+    }
+}
+</script>
+
+<style scoped>
+    .output-line{
+        display: grid;
+        grid-template-columns: 1fr 7fr 1fr 1fr;
+    }
+</style>
