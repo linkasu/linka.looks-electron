91c3fd39957c76ef5d9b660ab2d7ed2e158a28f4
Ivan Bakaidov <ibakaidov@ya.ru>
2023-04-05 15:27:45 +0300

    remove publish github

--- vue.config.js
+++ vue.config.js
@@ -8,14 +8,10 @@ module.exports = defineConfig({
   },
   pluginOptions: {
     electronBuilder: {
-      
-      // externals: ['ultimate-text-to-image'],
       nodeIntegration: true,
       builderOptions: {
-        publish: ['github'],
         extraResources: [
           {
-
             from: "./extraResources/",
             to: "extraResources",
             filter: ["**/*"],
