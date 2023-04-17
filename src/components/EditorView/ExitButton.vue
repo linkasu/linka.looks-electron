7c07399c9e17ce8891cab28550028c753667f003
Ivan Bakaidov <ibakaidov@ya.ru>
2023-04-17 11:29:45 +0300

    add exit from editor

---
+++ src/components/EditorView/ExitButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-dialog v-model="dialog" width="auto">
+    <template v-slot:activator="{ props }">
+      <v-btn flat icon="" v-bind="props">
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+    </template>
+
+    <v-card min-width="300px">
+      <v-card-title primary-title> Выйти без сохранения? </v-card-title>
+      <v-card-text> Вы уверены? </v-card-text>
+      <v-card-actions>
+        <v-btn
+          color="error"
+          @click="
+            $emit('exit');
+            dialog = false;
+          "
+          >Да</v-btn
+        >
+        <v-btn color="primary" @click="dialog = false">Нет</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Vue, prop, Options } from "vue-class-component";
+
+class Props {
+}
+
+@Options({})
+export default class ExitButton extends Vue.with(Props) {
+  dialog = false;
+}
+</script>
--- src/views/EditorView.appbar.vue
+++ src/views/EditorView.appbar.vue
@@ -1,48 +1,45 @@
 <template>
-    <v-app-bar>
-        <v-btn  icon  >
-            <v-icon>mdi-arrow-left</v-icon>
-        </v-btn>
-        <v-app-bar-title>
-            {{title}}
-        </v-app-bar-title>
-        <v-spacer/>
-        <v-btn  icon  >
-            <save-button :title="title" @save="save"/>
-        </v-btn>
-    </v-app-bar>
+  <v-app-bar>
+    <exit-button @exit="$router.back()"/>
+    <v-app-bar-title>
+      {{ title }}
+    </v-app-bar-title>
+    <v-spacer />
+    <v-btn icon>
+      <save-button :title="title" @save="save" />
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script lang="ts">
-import {Vue, prop, Options} from 'vue-class-component'
+import { Vue, prop, Options } from "vue-class-component";
 import SaveButton from "@/components/EditorView/SaveButton.vue";
-import { storageService } from '@/CardsStorage/frontend';
+import ExitButton from "@/components/EditorView/ExitButton.vue";
+import { storageService } from "@/CardsStorage/frontend";
 
-class Props{
-
-}
+class Props {}
 
 @Options({
-components:{
-
-    SaveButton
-}
+  components: {
+    ExitButton,
+    SaveButton,
+  },
 })
-export default class EditorViewAppBar extends Vue.with(Props){
- get path(): string {
-    return this.$store.getters.editor_current
+export default class EditorViewAppBar extends Vue.with(Props) {
+  get path(): string {
+    return this.$store.getters.editor_current;
   }
   get filename(): string | null {
-    return this.$store.getters.editor_temp
-  };
-  
+    return this.$store.getters.editor_temp;
+  }
+
   get title() {
     const arr = this.path.split("§");
     return arr[arr.length - 1];
   }
-  async save(){
-    await this.$store.dispatch('editor_save')
-    this.$router.back()
+  async save() {
+    await this.$store.dispatch("editor_save");
+    this.$router.back();
   }
 }
 </script>
