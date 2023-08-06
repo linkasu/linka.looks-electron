42745a5b97837baadd88693f1e8d5a087300dada
Ivan Bakaidov <ivan@aacidov.ru>
2023-08-06 14:54:51 +0300

    fix documents path

--- src/CardsStorage/constants.ts
+++ src/CardsStorage/constants.ts
@@ -1,4 +1,5 @@
+import { app } from 'electron';
 import {join} from 'path'
-const documentsPath = join(require('os').homedir(), 'Documents');
+const documentsPath = app.getPath("documents");
 const linkedFolderName = 'LINKa';
 export const HOME_DIR = join(documentsPath, linkedFolderName);
