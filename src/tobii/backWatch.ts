8fec9f7fedfa2826945934b28e2af3f1b25effb6
Ivan Bakaidov <ibakaidov@ya.ru>
2023-08-15 11:20:15 +0300

    fix big

--- src/tobii/backWatch.ts
+++ src/tobii/backWatch.ts
@@ -6,36 +6,36 @@ import { PageElementsState } from "../interfaces/PageElementsState";
 import { Bound } from "eyelog/dist/bound";
 
 export class BackWatch {
-    tobii?:TobiiProcess = undefined
+    tobii?: TobiiProcess = undefined
     window?: BrowserWindow;
     hid = ''
     constructor(win: BrowserWindow) {
         this.window = win;
         if (platform() === 'win32') {
             this.tobii = new TobiiProcess(join(__dirname, '.\\..\\extraResources\\bin\\EyeLog.exe'));
-            this.tobii?.on('enter', this.onEnter)
-            this.tobii?.on('exit', this.onExit)
-            this.tobii?.on('click', this.onClick)
+            this.tobii?.on('enter', (index: number) => this.onEnter(index))
+            this.tobii?.on('exit', () => this.onExit())
+            this.tobii?.on('click', (index, count) => this.onClick(index, count))
             ipcMain.on('eye-elements', (event, data: PageElementsState) => {
                 this.hid = data.id
                 const winBounds = win.getContentBounds()
-                const scale = screen.getPrimaryDisplay().scaleFactor 
-                
+                const scale = screen.getPrimaryDisplay().scaleFactor
+
                 const bounds: Bound[] = data.bounds.map((el) => {
-                    return Bound.fromArray([el.x + winBounds.x, el.y + winBounds.y, el.width, el.height].map(el=>Math.round(el*scale)))
-                }) 
-                
+                    return Bound.fromArray([el.x + winBounds.x, el.y + winBounds.y, el.width, el.height].map(el => Math.round(el * scale)))
+                })
+
                 this.tobii?.setBounds(bounds)
             })
-            ipcMain.on('button_timeout', (event, value)=>{
+            ipcMain.on('button_timeout', (event, value) => {
                 this.tobii?.setTimeout(value)
-                
+
             })
-                        
+
         }
     }
     onClick(index: number, count: number) {
-        if(!this.window?.isFocused()) return
+        if (!this.window?.isFocused()) return
         this.window?.webContents.send("eye-click", {
             elementIndex: index,
             count,
