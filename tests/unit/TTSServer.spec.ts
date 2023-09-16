4cce12b83cd2b5f05a1be1ab45ad91c181e59989
Ivan Bakaidov <ibakaidov@ya.ru>
2023-09-16 15:39:53 +0300

    fix test

--- tests/unit/TTSServer.spec.ts
+++ tests/unit/TTSServer.spec.ts
@@ -7,7 +7,7 @@ describe('tts function', () => {
 
   it('should return a buffer', async () => {
     const buffer = await tts('hello', 'alena');
-    expect(buffer).instanceOf(ArrayBuffer);
+    expect(buffer).instanceOf(Buffer);
     
   });
 
