dfd6994504100d52a7085cbb8d7155479e0b0093
Ivan Bakaidov <ibakaidov@ya.ru>
2023-11-18 17:51:29 +0300

    v2.3.1

--- src/frontend/utils/TTS.ts
+++ src/frontend/utils/TTS.ts
@@ -1,35 +1,41 @@
 import { storageService } from "@/frontend/services/card-storage-service";
 import { Card, CardType } from "@/common/interfaces/ConfigFile";
 import { tts } from "./TTSServer";
-import axios from "axios";
 
 export class TTS {
   isPlaying = false;
   audio = new Audio();
   private static _instance: TTS;
-  static get instance(): TTS {
+  static get instance (): TTS {
     if (TTS._instance == null) {
       TTS._instance = new TTS();
     }
     return TTS._instance;
   }
 
-  public async playCards(file: string, cards: Card[], force = false) {
+  public async playCards (file: string, cards: Card[], force = false) {
     if (this.isPlaying) {
       this.isPlaying = false;
       this.audio.pause();
       if (!force) return;
     }
     this.isPlaying = true;
-    let text = cards.map(({ title }) => title ?? '').join(' ')
-    const result = await this.normilize(text)
-    
-    await this.playText(result, 'alena', true)
+    for (const card of cards) {
+      if (!this.isPlaying) break;
+      if (card.cardType === CardType.AudioCard && card.audioPath) {
+        const buffer = await storageService.getAudio(file, card.audioPath);
+        if (!buffer) continue;
+        const url = URL.createObjectURL(
+          new Blob([buffer], { type: "audio/wav" } /* (1) */)
+        );
+        await this.playUrl(url);
+      }
+    }
     this.isPlaying = false;
   }
 
-  public async playText(text: string, voice = "alena", force = false): Promise<void> {
-    if (this.isPlaying&&!force) { this.audio.pause(); return; }
+  public async playText (text: string, voice = "alena"): Promise<void> {
+    if (this.isPlaying) { this.audio.pause(); return; }
     this.isPlaying = true;
     const buffer = await tts(text, voice);
     const url = URL.createObjectURL(
@@ -39,7 +45,7 @@ export class TTS {
     this.isPlaying = false;
   }
 
-  private playUrl(url: string) {
+  private playUrl (url: string) {
     return new Promise((resolve, reject) => {
       this.audio.src = url;
       this.audio.oncanplay = async () => {
@@ -47,8 +53,5 @@ export class TTS {
       };
       this.audio.onended = resolve;
     });
-  }
-  private async normilize(phrase: string) {
-    return (await axios.post<{ result: string; }>('https://declener.linka.su/normalize', { phrase })).data.result
   }
 }
